030f6a06a1faffba9ec8e9bf192174d65656c9da
Carlos Echeverria <echeverriadev@gmail.com>
2020-05-29 11:51:23 -0400

    funciones

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 console.log('Funciones:');
+//Funciones simples
+function simpleFun() {
+    console.log('Funcion simple');
+}
+simpleFun();
+//Funciones con parametros
+function paramFun(name) {
+    console.log(name);
+}
+paramFun('Carlos');
+//Funciones con retorno de datos
+function retornaDatos(num1, num2) {
+    return num1 + num2;
+}
+console.log(retornaDatos(1, 3));
--- app.ts
+++ app.ts
@@ -1,1 +1,1 @@
-console.log('Funciones:');
+console.log('Classes');
